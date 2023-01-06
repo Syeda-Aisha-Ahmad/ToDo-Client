38d3300cad49983f8e2445bd8866657dbb7edc06
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2023-01-07 02:23:15 +0600

    bottom navbar

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -5,23 +5,25 @@ import { GrAdd } from 'react-icons/gr';
 
 const Footer = () => {
     return (
-        <div className="btm-nav h-20 border border-gray-500">
-            <button>
-                <ImSearch className='mr-5 text-2xl' />
-            </button>
-            <button className="">
-                <ImHome className='mr-5 text-2xl' />
-            </button>
-            <button className='bg-indigo-400 mr-5 my-2 rounded-full'>
-                <GrAdd className='text-2xl font-bold' />
-            </button>
-            <button>
-                <ImCalendar className='mr-5 text-2xl' />
-            </button>
-            <button>
-                <MdDelete className='mr-5 text-2xl' />
-            </button>
-        </div>
+        <footer className=''>
+            <div className="btm-nav h-20 border border-gray-500">
+                <button>
+                    <ImSearch className='mr-5 text-2xl' />
+                </button>
+                <button className="">
+                    <ImHome className='mr-5 text-2xl' />
+                </button>
+                <button className='bg-indigo-400 h-16 mr-5 my-2 rounded-full'>
+                    <GrAdd className='text-2xl font-bold p-0' />
+                </button>
+                <button>
+                    <ImCalendar className='mr-5 text-2xl' />
+                </button>
+                <button>
+                    <MdDelete className='mr-5 text-2xl' />
+                </button>
+            </div>
+        </footer>
     );
 };
 
